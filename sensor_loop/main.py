c40378f42f65ca9b29fb32a78da948ad2976b004
Brendon Matheson <b@brendonmatheson.com>
2021-05-23 07:13:16 +0100

    Added publishing of BME280 measurements to the MQTT broker

--- sensor_loop/main.py
+++ sensor_loop/main.py
@@ -1,12 +1,33 @@
 import bme280
+import paho.mqtt.client as mqtt
 import smbus2
 from time import sleep
+
+def on_connect(client, userdata, flags, rc):
+	if rc == 0:
+		print("Connected to MQTT broker")
+	else:
+		print("Failed to connect")
+
+# Configure MQTT CLient
+broker = "localhost"
+port = 1883
+client_id = "hea92weather01"
+username = "hea92weather01"
+password = "password"
+
+client = mqtt.Client(client_id)
+client.username_pw_set(username, password)
+client.on_connect = on_connect
+client.connect(broker, port)
 
 port = 1
 address = 0x76
 bus = smbus2.SMBus(port)
 
 bme280.load_calibration_params(bus,address)
+
+base_topic = "hea92weather01"
 
 while True:
 	bme280_data = bme280.sample(bus, address)
@@ -16,7 +37,9 @@ while True:
 
 	print(humidity, pressure, temperature)
 
-	sleep(1)
+	client.publish(base_topic + "/humidity", humidity)
+	client.publish(base_topic + "/pressure", pressure)
+	client.publish(base_topic + "/temperature", temperature)
 
-print("hi bren")
+	sleep(1)
 
