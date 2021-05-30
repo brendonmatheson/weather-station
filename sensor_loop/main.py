be7a4638017ca433ddb19931e8b5bbed32c0fad7
Brendon Matheson <b@brendonmatheson.com>
2021-05-30 05:34:30 +0100

    WST-30 Made sensor_loop respond to SIGINT and SIGTERM to gracefully end

--- sensor_loop/main.py
+++ sensor_loop/main.py
@@ -1,70 +1,93 @@
 import bme280
 import paho.mqtt.client as mqtt
+import signal
 import smbus2
 import SI1145.SI1145 as SI1145
 from time import sleep
 
-def on_connect(client, userdata, flags, rc):
+def mqtt_on_connect(client, userdata, flags, rc):
 	if rc == 0:
 		print("Connected to MQTT broker")
 	else:
 		print("Failed to connect")
 
-# Configure MQTT CLient
-broker = "localhost"
-port = 1883
-client_id = "hea92weather01"
-username = "hea92weather01"
-password = "password"
+class SensorReader:
 
-client = mqtt.Client(client_id)
-client.username_pw_set(username, password)
-client.on_connect = on_connect
-client.connect(broker, port)
+	def __init__(self):
+		self.stopped = False
 
-#
-# BME280
-#
+	def run(self):
 
-port = 1
-address = 0x76
-bus = smbus2.SMBus(port)
+		#
+		# Configure MQTT CLient
+		#
 
-bme280.load_calibration_params(bus,address)
+		broker = "localhost"
+		port = 1883
+		client_id = "hea92weather01"
+		username = "hea92weather01"
+		password = "password"
 
-#
-# GY1145 / SI1145
-#
+		client = mqtt.Client(client_id)
+		client.username_pw_set(username, password)
+		client.on_connect = mqtt_on_connect
+		client.connect(broker, port)
 
-si1145 = SI1145.SI1145()
+		#
+		# BME280
+		#
 
-base_topic = "hea92weather01"
+		port = 1
+		address = 0x76
+		bus = smbus2.SMBus(port)
 
-while True:
+		bme280.load_calibration_params(bus,address)
 
-	# BME280
-	bme280_data = bme280.sample(bus, address)
-	humidity = bme280_data.humidity
-	pressure = bme280_data.pressure
-	temperature = bme280_data.temperature
+		#
+		# GY1145 / SI1145
+		#
 
-	# SI1145
-	lightVisible = si1145.readVisible()
-	lightUV = si1145.readUV()
-	lightIR = si1145.readIR()
+		si1145 = SI1145.SI1145()
 
-	# Publish
-	line = "weather,station=hea92weather01 " + \
-		"humidity=" + str(humidity) + \
-		",pressure=" + str(pressure) + \
-		",temperature=" + str(temperature) + \
-		",visible=" + str(lightVisible) + \
-		",uv=" + str(lightUV) + \
-		",ir=" + str(lightIR)
+		base_topic = "hea92weather01"
 
-	print(line)
+		while not self.stopped:
+			# BME280
+			bme280_data = bme280.sample(bus, address)
+			humidity = bme280_data.humidity
+			pressure = bme280_data.pressure
+			temperature = bme280_data.temperature
 
-	client.publish("weather", line)
+			# SI1145
+			lightVisible = si1145.readVisible()
+			lightUV = si1145.readUV()
+			lightIR = si1145.readIR()
 
-	sleep(1)
+			# Publish
+			line = "weather,station=hea92weather01 " + \
+				"humidity=" + str(humidity) + \
+				",pressure=" + str(pressure) + \
+				",temperature=" + str(temperature) + \
+				",visible=" + str(lightVisible) + \
+				",uv=" + str(lightUV) + \
+				",ir=" + str(lightIR)
+
+			print(line)
+ 
+			client.publish("weather", line)
+
+			sleep(1)
+
+	def stop(self, signal, frame):
+		print("Stopping SensorReader")
+		self.stopped = True
+
+def main():
+	sensorReader = SensorReader()
+	signal.signal(signal.SIGINT, sensorReader.stop)
+	signal.signal(signal.SIGTERM, sensorReader.stop)
+	sensorReader.run()
+
+if __name__ == "__main__":
+	main()
 
